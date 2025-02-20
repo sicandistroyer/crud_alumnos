c2fda50e96bdc57f163dd2e0ade8614f6dca4719
Jaime Rodriguez <rpjosejaime@gmail.com>
2025-02-20 15:23:28 -0600

    FIX: Se agrego variables de entorno

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+import os
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv 
+
+#Cargar las variables de entorno
+load_dotenv()
 
 #crear instancia
 app =  Flask(__name__)
 
 # Configuración de la base de datos PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://josejaime:AdSAzoWk5EPHiiifdR8FYHFnMkiwC9fO@dpg-cuiikhq3esus739hsdmg-a.oregon-postgres.render.com/db_cetech_rvoj'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
